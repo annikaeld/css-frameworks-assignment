c474e9186daddc702650f8966dcec8cedc030d50
annikaeld <130365506+annikaeld@users.noreply.github.com>
2025-02-19 04:34:14 +0100

    Mobile menu and favicon

--- test.js
+++ test.js
@@ -6,3 +6,17 @@ function myFunction() {
     x.type = "password";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mobileOpenButton = document.getElementById("mobile-open-button");
+  if (mobileOpenButton) {
+    mobileOpenButton.addEventListener("click", function () {
+      var nav = document.querySelector('nav[aria-label="main"]');
+      if (nav) {
+        nav.classList.toggle("hidden");
+      }
+    });
+  } else {
+    console.error("Element with ID 'mobile-open-button' not found");
+  }
+});
